c626969ae5422daada39461d4b92d660d625d572
GitHub Action <action@github.com>
2025-10-01 05:21:20 +0700

    update

--- save_photo.php
+++ save_photo.php
@@ -1,15 +1,19 @@
 <?php
-require_once "classes/Photo.php";
-require_once "classes/Logger.php";
 require_once "classes/Controllers/PhotoController.php";
-require_once "classes/Traits/Timestampable.php";
-require_once "classes/Interfaces/Savable.php";
 
 use Controllers\PhotoController;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['photo'])) {
     $controller = new PhotoController();
     $filename = $controller->save($_POST['photo']);
-    echo "<p>Foto berhasil disimpan: $filename</p>";
-    echo "<a href='gallery.php'>Lihat Galeri</a>";
+
+    if ($filename) {
+        echo "<p>Foto berhasil disimpan: $filename</p>";
+        echo "<a href='gallery.php'>Lihat Galeri Penyimpanan</a>";
+    } else {
+        echo "<p>Gagal menyimpan foto.</p>";
+    }
+} else {
+    echo "<p>Tidak ada data foto yang disimpan !.</p>";
 }
+?>
